41408986224d2de22a9d9cdba53f906d0ca78a35
luthfan hafizh <luthfanhafizh@luthfans-MacBook-Pro.local>
2025-09-03 15:05:31 +0700

    update sidebar hide in mobile

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,28 +1,21 @@
-import { Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from "@mui/material";
+import { Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import PeopleIcon from "@mui/icons-material/People";
 import WorkIcon from "@mui/icons-material/Work";
 import BarChartIcon from "@mui/icons-material/BarChart";
 import { Link } from "react-router-dom";
+import React from "react";
 
 const drawerWidth = 240;
 
 const Sidebar = () => {
-  return (
-    <Drawer
-      variant="permanent"
-      sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        "& .MuiDrawer-paper": {
-          width: drawerWidth,
-          boxSizing: "border-box",
-          backgroundColor: "#0D47A1", // Biru tua
-          color: "#fff"
-        },
-      }}
-    >
-      <Toolbar>
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const isMobile = window.innerWidth < 1040;
+
+  const drawerContent = (
+    <>
+    <Toolbar>
         <h2 style={{ fontWeight: "bold" }}>Company</h2>
       </Toolbar>
       <List>
@@ -51,7 +44,60 @@ const Sidebar = () => {
           </ListItemButton>
         </ListItem>
       </List>
-    </Drawer>
+    </>
+  );
+  return (
+    <>
+    {isMobile ? (
+        <>
+          {/* Tombol menu untuk mobile */}
+            <IconButton
+            onClick={() => setMobileOpen(true)}
+            sx={{
+              position: "absolute",
+              top: 10,
+              left: 10,
+              color: "black",
+              zIndex: 2000,
+              display: mobileOpen ? "none" : "block",
+            }}
+            >
+            <MenuIcon />
+            </IconButton>
+
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={() => setMobileOpen(false)}
+            sx={{
+              "& .MuiDrawer-paper": {
+                width: drawerWidth,
+                backgroundColor: "#0D47A1",
+                color: "#fff",
+              },
+            }}
+          >
+            {drawerContent}
+          </Drawer>
+        </>
+      ) : (
+        <Drawer
+          variant="permanent"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+              backgroundColor: "#0D47A1", // Biru tua
+              color: "#fff"
+            },
+          }}
+        >
+          {drawerContent}
+        </Drawer>
+      )}
+    </>
   );
 };
 
